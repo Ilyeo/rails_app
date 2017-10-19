b7afdfad141564f66463fe7b6677190dc746cbaf
Ilyeo <jrag.tkd@gmail.com>
2017-10-19 13:38:17 -0500

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test 'invalid signup information' do
+    get signup_path
+    # before_count = User.count
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '', email: 'user@invalid', password: 'foo', password_confirmation: 'bar' } }
+    end
+    # after_count = User.count
+    # assert_equal before_count, after_count
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+  end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: { user: { name: 'Example user', email: 'user@example.com', password: 'password', password_confirmation: 'password' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
